f90860a2381aeaf315d2dbe284e55e249543cb48
CristianCaro915 <c.caro@uniandes.edu.co>
2024-09-04 21:33:12 -0500

    Initial commit

---
+++ XcodeTry/XcodeTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTryApp.swift
+//  XcodeTry
+//
+//  Created by Cristian Caro on 4/09/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
